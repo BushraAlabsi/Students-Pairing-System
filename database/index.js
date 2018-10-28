6e5076ab0fd1f52483f41e370a125eb695fe6371
BushraAlabsi <boushra-93@live.com>
2018-10-28 19:23:53 +0200

    deploy database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 
- mongoose.connect('mongodb://localhost:27017/pairing', { useNewUrlParser: true, 
+ mongoose.connect('mongodb://bushraalabsi:abcd1234@ds117469.mlab.com:17469/pairing', { useNewUrlParser: true, 
 
   useCreateIndex: true
  });
